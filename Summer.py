6d4c5bfa60039fb63210c2febe90bf62c336ba6f
ggpradeep <80970765+ggpradeep@users.noreply.github.com>
2026-05-13 18:21:48 -0700

    Add files via upload

---
+++ Summer.py
@@ -0,0 +1,9 @@
+a = 70
+b = 90
+c = int(input("Enter a number"))
+
+if c < a or c > b:
+    print("The temprature is too hot or too cold")
+else:
+    print("The temprature will be suitable")
+#This is in farhienheit
